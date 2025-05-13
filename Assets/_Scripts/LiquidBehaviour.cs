662e3016e87c0aafecf86565483e0aa21ae092dd
Nowogórska Laura <242479@edu.p.lodz.pl>
2025-05-13 13:26:36 +0200

    [Added] DOTween, cylinder for simulating water/coffee

---
+++ Assets/_Scripts/LiquidBehaviour.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class LiquidBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject liquidObject;
+    [SerializeField] private float topScale;
+
+    public void PourLiquid()
+    {
+        liquidObject.SetActive(true);
+        liquidObject.transform.;
+    }
+}
